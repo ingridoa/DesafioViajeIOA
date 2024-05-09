d39815ec1d21ff331ca510f6dfff4eb0a1d3ac5f
Ingrid <ingrid.oa@gmail.com>
2024-05-08 21:01:37 -0400

    cambios de Modal y mensajes de alerta

---
+++ README.md
@@ -0,0 +1,3 @@
+## Desafio chile Mejorado JajaJa!
+## Se trabajo con tamaños 1280X800px
+## se uso Pantalla Movile !iphone SE
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+document.addEventListener("DOMContentLoaded", function () {
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  );
+});
+
+$(document).ready(function(){
+    $("#buttonSend").click(function(){
+  
+   alert("¡Tu mensaje se ha enviado!");
+ });
+});
+
+
+//Script para activacion de modal//
+
+document.addEventListener('DOMContentLoaded', () => {
+    const myModal = document.getElementById('myModal');
+    const myInput = document.getElementById('myInput');
+  
+    if (myModal) {
+      myModal.addEventListener('shown.bs.modal', () => {
+        myInput.focus();
+      });
+    }
   });
